b7fc9fa0939ed4944f32c70bc58df60163a0aabe
Sudano <joaohsudano@gmail.com>
2024-03-15 00:58:10 -0300

    added soup drink delay and recraft type

--- modules/combat/AutoSoup.java
+++ modules/combat/AutoSoup.java
@@ -1,12 +1,9 @@
 package elixe.modules.combat;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Random;
 
 import elixe.events.OnKeybindActionEvent;
-import elixe.events.OnPacketSendEvent;
 import elixe.events.OnTickEvent;
 import elixe.modules.Module;
 import elixe.modules.ModuleCategory;
@@ -14,28 +11,17 @@ import elixe.modules.option.ModuleArrayMultiple;
 import elixe.modules.option.ModuleBoolean;
 import elixe.modules.option.ModuleFloat;
 import elixe.modules.option.ModuleInteger;
-import elixe.utils.misc.ChatUtils;
 import elixe.utils.misc.TimerUtils;
 import elixe.utils.player.InventoryItem;
-import me.zero.alpine.event.EventPriority;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockBush;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemSword;
-import net.minecraft.network.play.client.C00PacketKeepAlive;
-import net.minecraft.network.play.client.C03PacketPlayer;
 import net.minecraft.network.play.client.C07PacketPlayerDigging;
-import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
-import net.minecraft.network.play.client.C09PacketHeldItemChange;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 
@@ -45,12 +31,14 @@ public class AutoSoup extends Module {
 		super("AutoSoup", ModuleCategory.COMBAT);
 
 		moduleOptions.add(healthToSoupOption);
+		moduleOptions.add(drinkDelayOption);
 		moduleOptions.add(dropBowlOption);
 
 		moduleOptions.add(refillOption);
 		moduleOptions.add(refillDelayOption);
 
 		moduleOptions.add(recraftOption);
+		moduleOptions.add(newRecraftOption);
 		moduleOptions.add(recraftableItemsOption);
 		moduleOptions.add(recraftDelayOption);
 
@@ -61,6 +49,13 @@ public class AutoSoup extends Module {
 	ModuleFloat healthToSoupOption = new ModuleFloat("health to soup", 12f, 1f, 20f) {
 		public void valueChanged() {
 			healthToSoup = (float) this.getValue();
+		}
+	};
+
+	int drinkDelay;
+	ModuleInteger drinkDelayOption = new ModuleInteger("drink delay", 100, 1, 300) {
+		public void valueChanged() {
+			drinkDelay = (int) this.getValue();
 		}
 	};
 
@@ -92,9 +87,16 @@ public class AutoSoup extends Module {
 		}
 	};
 
+	boolean newRecraft;
+	ModuleBoolean newRecraftOption = new ModuleBoolean("new recraft §e(BETA)§r", false) {
+		public void valueChanged() {
+			newRecraft = (boolean) this.getValue();
+		}
+	};
+
 	boolean[] recraftableItems;
-	ModuleArrayMultiple recraftableItemsOption = new ModuleArrayMultiple("recraftable items", new boolean[] { true, false, false },
-			new String[] { "mushroom", "cocoa", "cactus" }) {
+	ModuleArrayMultiple recraftableItemsOption = new ModuleArrayMultiple("recraftable items",
+			new boolean[] { true, false, false }, new String[] { "mushroom", "cocoa", "cactus" }) {
 		public void valueChanged() {
 			recraftableItems = (boolean[]) this.getValue();
 		}
@@ -116,10 +118,13 @@ public class AutoSoup extends Module {
 
 	// crafts
 	Object[][] combinationItems = { { Blocks.red_mushroom, Blocks.brown_mushroom }, { Items.dye }, { Blocks.cactus } };
-	Object[][] bakedCombinations = { { Blocks.red_mushroom, Blocks.brown_mushroom }, { Blocks.brown_mushroom, Blocks.red_mushroom }, { Items.dye }, { Blocks.cactus } };
+	Object[][] bakedCombinations = { { Blocks.red_mushroom, Blocks.brown_mushroom },
+			{ Blocks.brown_mushroom, Blocks.red_mushroom }, { Items.dye }, { Blocks.cactus } };
 
 	boolean autoSouping = false;
 	int autoSoupStep = 0;
+
+	TimerUtils.MilisecondTimer drinkTimer = new TimerUtils().new MilisecondTimer();
 
 	TimerUtils.MilisecondTimer refillTimer = new TimerUtils().new MilisecondTimer();
 
@@ -135,24 +140,6 @@ public class AutoSoup extends Module {
 	int soupInHotbar;
 	int soupInInventory;
 
-//	@EventHandler
-//	private Listener<OnPacketSendEvent> onPacketSendEvent = new Listener<>(e -> {
-//		if (e.getPacket() instanceof C09PacketHeldItemChange) {
-//			System.out.println("mudança de slot -> " + ((C09PacketHeldItemChange) e.getPacket()).getSlotId());
-//		} else if (e.getPacket() instanceof C08PacketPlayerBlockPlacement) {
-//			C08PacketPlayerBlockPlacement placement = (C08PacketPlayerBlockPlacement) e.getPacket();
-//			if (placement.getPosition().getY() == -1) {
-//				System.out.println("item usado -> " + placement.getStack());
-//			} else {
-//				System.out.println("item usado em bloco -> " + placement.getStack());
-//			}
-//
-//		} else if (e.getPacket() instanceof C07PacketPlayerDigging) {
-//			System.out.println("ação no item -> " + ((C07PacketPlayerDigging) e.getPacket()).getStatus());
-//		}
-//
-//	});
-
 	@EventHandler
 	private Listener<OnTickEvent> onTickEvent = new Listener<>(e -> {
 		if (mc.currentScreen instanceof GuiInventory) {
@@ -162,7 +149,10 @@ public class AutoSoup extends Module {
 
 				if (refill) { // opcao de refill ativada e nao ta fazendo recraft
 					if (refillTimer.hasTimePassed(refillDelay)) { // timer passou
-						soupInInventory = InventoryItem.findItemRandomized(9, 36, Items.mushroom_stew, mc); // tenta pegar sopa no inv
+						soupInInventory = InventoryItem.findItemRandomized(9, 36, Items.mushroom_stew, mc); // tenta
+																											// pegar
+																											// sopa no
+																											// inv
 						if (soupInInventory != -1 && InventoryItem.hasSpaceHotbar(mc)) { // tem sopa e espaço na hotbar
 							mc.playerController.windowClick(0, soupInInventory, 0, 1, mc.thePlayer);
 							refillTimer.reset();
@@ -191,7 +181,8 @@ public class AutoSoup extends Module {
 								if (recraftableItems[i]) {
 									for (int j = 0; j < combinationItems[i].length; j++) {
 										if (combinationItems[i][j] instanceof Block) { // block
-											InventoryItem block = InventoryItem.findBlockHighest(9, 45, (Block) combinationItems[i][j], mc);
+											InventoryItem block = InventoryItem.findBlockHighest(9, 45,
+													(Block) combinationItems[i][j], mc);
 											if (block.getSlot() != -1) {
 												sortedItems.add(block);
 											}
@@ -203,7 +194,8 @@ public class AutoSoup extends Module {
 											// eu nao fiz uma classe especifica pra isso, entao assumo que só pode ser
 											// cocoa bean
 											// no futuro, se precisar, refatoro
-											InventoryItem item = InventoryItem.findItemHighest(9, 45, (Item) combinationItems[i][j], 3, mc);
+											InventoryItem item = InventoryItem.findItemHighest(9, 45,
+													(Item) combinationItems[i][j], 3, mc);
 											if (item.getSlot() != -1) {
 												sortedItems.add(item);
 											}
@@ -226,11 +218,12 @@ public class AutoSoup extends Module {
 									boolean canCraft = true;
 									// todos os items da combinacao atual
 									for (int k = 0; k < bakedCombinations[j].length; k++) {
-										//combinacao len + index do loop for maior que itens
+										// combinacao len + index do loop for maior que itens
 										if (bakedCombinations[j].length + i > sortedItems.size()) {
 											canCraft = false;
 										} else {
-											//se index atual do loop de itens + index do loop da combinacao nao sao da combinacao
+											// se index atual do loop de itens + index do loop da combinacao nao sao da
+											// combinacao
 											if (sortedItems.get(i + k).getObject() != bakedCombinations[j][k]) {
 												canCraft = false;
 											}
@@ -273,79 +266,144 @@ public class AutoSoup extends Module {
 	private void makeRecraftStep() {
 		if (recraftTimer.hasTimePassed(recraftDelay)) {
 
-			switch (recraftStep) {
-			case 1: // pega primeiro item do recraft
-				mc.playerController.windowClick(0, itemsToUse[0].getSlot(), 0, 0, mc.thePlayer);
-				if (itemsToUse[0].getSize() == 1) { // se for só 1 item, pula step de botao direito
-					recraftStep++;
+			if (newRecraft) {
+				switch (recraftStep) {
+				case 1: // pega primeiro item do recraft
+					mc.playerController.windowClick(0, itemsToUse[0].getSlot(), 1, 0, mc.thePlayer);
+					if (itemsToUse[0].getSize() == 1) { // se for só 1 item, pula step de botao direito
+						recraftStep++;
+					}
+					break;
+
+				case 2: // coloca o item na crafting
+					mc.playerController.windowClick(0, 3, 0, 0, mc.thePlayer);
+					// checa se craft é 1 item só
+					if (itemsToUse[1] == null) {
+						recraftStep += 3; // pula pro step 7
+					}
+					break;
+
+				case 3:
+					mc.playerController.windowClick(0, itemsToUse[1].getSlot(), 1, 0, mc.thePlayer);
+					if (itemsToUse[1].getSize() == 1) {
+						recraftStep++;
+					}
+					break;
+
+				case 5:
+					mc.playerController.windowClick(0, 4, 0, 0, mc.thePlayer);
+					break;
+
+				case 6: // msm coisa mas com pote
+					mc.playerController.windowClick(0, bowlRecraft.getSlot(), 1, 0, mc.thePlayer);
+					if (bowlRecraft.getSize() == 1) {
+						recraftStep++;
+					}
+					break;
+
+				case 7:
+					mc.playerController.windowClick(0, 2, 0, 0, mc.thePlayer);
+					break;
+
+				case 8: // shift na crafting pra fazer tudo de sopa
+					mc.playerController.windowClick(0, 0, 0, 1, mc.thePlayer);
+					break;
+
+				case 9: // shift na crafting pra tirar os itens
+					if (mc.thePlayer.inventoryContainer.getSlot(2).getStack() != null) {
+						mc.playerController.windowClick(0, 2, 0, 1, mc.thePlayer);
+					}
+					break;
+
+				case 10:
+					if (mc.thePlayer.inventoryContainer.getSlot(3).getStack() != null) {
+						mc.playerController.windowClick(0, 3, 0, 1, mc.thePlayer);
+					}
+					break;
+
+				case 11:
+					if (mc.thePlayer.inventoryContainer.getSlot(4).getStack() != null) {
+						mc.playerController.windowClick(0, 4, 0, 1, mc.thePlayer);
+					}
+					recrafting = false;
+					itemsToUse = new InventoryItem[2];
+					break;
 				}
-				break;
+			} else {
+				switch (recraftStep) {
+				case 1: // pega primeiro item do recraft
+					mc.playerController.windowClick(0, itemsToUse[0].getSlot(), 0, 0, mc.thePlayer);
+					if (itemsToUse[0].getSize() == 1) { // se for só 1 item, pula step de botao direito
+						recraftStep++;
+					}
+					break;
 
-			case 2: // clica uma vez com o direito pra colocar um item no slot
-				mc.playerController.windowClick(0, itemsToUse[0].getSlot(), 1, 0, mc.thePlayer);
-				break;
+				case 2: // clica uma vez com o direito pra colocar um item no slot
+					mc.playerController.windowClick(0, itemsToUse[0].getSlot(), 1, 0, mc.thePlayer);
+					break;
 
-			case 3: // coloca o item na crafting
-				mc.playerController.windowClick(0, 3, 0, 0, mc.thePlayer);
-				// checa se craft é 1 item só
-				if (itemsToUse[1] == null) {
-					recraftStep += 3; // pula pro step 7
+				case 3: // coloca o item na crafting
+					mc.playerController.windowClick(0, 3, 0, 0, mc.thePlayer);
+					// checa se craft é 1 item só
+					if (itemsToUse[1] == null) {
+						recraftStep += 3; // pula pro step 7
+					}
+					break;
+
+				case 4:
+					mc.playerController.windowClick(0, itemsToUse[1].getSlot(), 0, 0, mc.thePlayer);
+					if (itemsToUse[1].getSize() == 1) {
+						recraftStep++;
+					}
+					break;
+
+				case 5:
+					mc.playerController.windowClick(0, itemsToUse[1].getSlot(), 1, 0, mc.thePlayer);
+					break;
+
+				case 6:
+					mc.playerController.windowClick(0, 4, 0, 0, mc.thePlayer);
+					break;
+
+				case 7: // msm coisa mas com pote
+					mc.playerController.windowClick(0, bowlRecraft.getSlot(), 0, 0, mc.thePlayer);
+					if (bowlRecraft.getSize() == 1) {
+						recraftStep++;
+					}
+					break;
+
+				case 8:
+					mc.playerController.windowClick(0, bowlRecraft.getSlot(), 1, 0, mc.thePlayer);
+					break;
+
+				case 9:
+					mc.playerController.windowClick(0, 2, 0, 0, mc.thePlayer);
+					break;
+
+				case 10: // shift na crafting pra fazer tudo de sopa
+					mc.playerController.windowClick(0, 0, 0, 1, mc.thePlayer);
+					break;
+
+				case 11: // shift na crafting pra tirar os itens
+					if (mc.thePlayer.inventoryContainer.getSlot(2).getStack() != null) {
+						mc.playerController.windowClick(0, 2, 0, 1, mc.thePlayer);
+					}
+					break;
+
+				case 12:
+					if (mc.thePlayer.inventoryContainer.getSlot(3).getStack() != null) {
+						mc.playerController.windowClick(0, 3, 0, 1, mc.thePlayer);
+					}
+					break;
+
+				case 13:
+					if (mc.thePlayer.inventoryContainer.getSlot(4).getStack() != null) {
+						mc.playerController.windowClick(0, 4, 0, 1, mc.thePlayer);
+					}
+					recrafting = false;
+					itemsToUse = new InventoryItem[2];
+					break;
 				}
-				break;
-
-			case 4:
-				mc.playerController.windowClick(0, itemsToUse[1].getSlot(), 0, 0, mc.thePlayer);
-				if (itemsToUse[1].getSize() == 1) {
-					recraftStep++;
-				}
-				break;
-
-			case 5:
-				mc.playerController.windowClick(0, itemsToUse[1].getSlot(), 1, 0, mc.thePlayer);
-				break;
-
-			case 6:
-				mc.playerController.windowClick(0, 4, 0, 0, mc.thePlayer);
-				break;
-
-			case 7: // msm coisa mas com pote
-				mc.playerController.windowClick(0, bowlRecraft.getSlot(), 0, 0, mc.thePlayer);
-				if (bowlRecraft.getSize() == 1) {
-					recraftStep++;
-				}
-				break;
-
-			case 8:
-				mc.playerController.windowClick(0, bowlRecraft.getSlot(), 1, 0, mc.thePlayer);
-				break;
-
-			case 9:
-				mc.playerController.windowClick(0, 2, 0, 0, mc.thePlayer);
-				break;
-
-			case 10: // shift na crafting pra fazer tudo de sopa
-				mc.playerController.windowClick(0, 0, 0, 1, mc.thePlayer);
-				break;
-
-			case 11: // shift na crafting pra tirar os itens
-				if (mc.thePlayer.inventoryContainer.getSlot(2).getStack() != null) {
-					mc.playerController.windowClick(0, 2, 0, 1, mc.thePlayer);
-				}
-				break;
-
-			case 12:
-				if (mc.thePlayer.inventoryContainer.getSlot(3).getStack() != null) {
-					mc.playerController.windowClick(0, 3, 0, 1, mc.thePlayer);
-				}
-				break;
-
-			case 13:
-				if (mc.thePlayer.inventoryContainer.getSlot(4).getStack() != null) {
-					mc.playerController.windowClick(0, 4, 0, 1, mc.thePlayer);
-				}
-				recrafting = false;
-				itemsToUse = new InventoryItem[2];
-				break;
 			}
 			recraftStep++;
 			recraftTimer.reset();
@@ -373,30 +431,34 @@ public class AutoSoup extends Module {
 	});
 
 	private void makeAutoSoupStep() {
-		switch (autoSoupStep) {
-		case 1: // toma a sopa
-			// tenta clicar com o direito
-			autoSoupStep++;
-			waitForUseItem = true;
-			break;
-			
-		case 2: // dropa o pote
-			// se o direito conseguir clicar
-			if (!waitForUseItem) {
-				mc.thePlayer.sendQueue.addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.DROP_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN));
+		if (drinkTimer.hasTimePassed(drinkDelay)) {
+			switch (autoSoupStep) {
+			case 1: // toma a sopa
+				// tenta clicar com o direito
 				autoSoupStep++;
-			}
-			break;
+				waitForUseItem = true;
+				break;
 
-		case 3:
-			if (!waitForUseItem) {
-				if (!shouldAutoSoup(false)) { // se puder continuar o loop sem voltar pro item inicial
-					mc.thePlayer.inventory.currentItem = lastItem;
-					autoSoupStep = 1; // seta step pro inicio
-					autoSouping = false; // para loop
+			case 2: // dropa o pote
+				// se o direito conseguir clicar
+				if (!waitForUseItem) {
+					mc.thePlayer.sendQueue.addToSendQueue(new C07PacketPlayerDigging(
+							C07PacketPlayerDigging.Action.DROP_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN));
+					autoSoupStep++;
 				}
+				break;
+
+			case 3:
+				if (!waitForUseItem) {
+					if (!shouldAutoSoup(false)) { // se puder continuar o loop sem voltar pro item inicial
+						mc.thePlayer.inventory.currentItem = lastItem;
+						autoSoupStep = 1; // seta step pro inicio
+						autoSouping = false; // para loop
+					}
+				}
+				break;
 			}
-			break;
+			drinkTimer.reset();
 		}
 	}
 
@@ -420,7 +482,4 @@ public class AutoSoup extends Module {
 		return false;
 	}
 
-	
-
-	
 }
